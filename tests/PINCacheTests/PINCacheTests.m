688223501ceef13194a3dae85936a319532a44af
Garrett Moon <garrett@pinterest.com>
2016-08-25 18:24:54 -0700

    Adds PINOperationQueue
    PINOperationQueue is an operation queue designed to allow
    specifying the maximum number of threads, support priority (ordering,
    not thread priority and cancelation).
    
    It is also designed to prevent live locks due to priority inversion.
    It attempts to achieve this by maintaining two queues: a serial queue
    which always takes operations in FIFO order and a concurrent queue
    which runs operations in priority order.

--- tests/PINCacheTests/PINCacheTests.m
+++ tests/PINCacheTests/PINCacheTests.m
@@ -6,7 +6,7 @@
 #import "PINCache.h"
 
 static NSString * const PINCacheTestName = @"PINCacheTest";
-static const NSTimeInterval PINCacheTestBlockTimeout = 10.0;
+const NSTimeInterval PINCacheTestBlockTimeout = 10.0;
 
 @interface PINDiskCache()
 
